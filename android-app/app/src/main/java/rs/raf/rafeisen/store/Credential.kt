eb617c805fbdf8debf81d8b45c68208fd81fb4e4
Arsen Arsenović <arsen@aarsen.me>
2025-03-20 22:00:42 +0100

    android: make Credential serializable

--- android-app/app/src/main/java/rs/raf/rafeisen/store/Credential.kt
+++ android-app/app/src/main/java/rs/raf/rafeisen/store/Credential.kt
@@ -1,12 +1,10 @@
 package rs.raf.rafeisen.store
 
-import java.time.Instant
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class Credential(
     val id: String,
     val accessToken: String,
-    val refreshToken: String,
-    val expiresAt: Long = Instant.now().plusSeconds(150).epochSecond,
-) {
-    fun isExpired() = Instant.ofEpochSecond(expiresAt).isBefore(Instant.now())
-}
+    val refreshToken: String
+)
