55c8b1b27da3c853a918ea9df434a474b9aa497f
Arsen Arsenović <arsen@aarsen.me>
2025-03-20 22:02:16 +0100

    android: add a removePrefixIgnoreCase utility

---
+++ android-app/app/src/main/java/rs/raf/rafeisen/utils/StringUtils.kt
@@ -0,0 +1,7 @@
+package rs.raf.rafeisen.utils
+
+fun String.removePrefixIgnoreCase(s: String): String =
+    if (startsWith(s, ignoreCase = true))
+        substring(s.length)
+    else
+        this
