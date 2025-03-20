8688d2679d097dcaff2fcf77c7b6c3702388f999
Marko Kocic <markokocic2000@gmail.com>
2025-03-21 00:15:41 +0100

    android: implement splash screen

--- android-app/app/src/main/java/rs/raf/rafeisen/MainActivity.kt
+++ android-app/app/src/main/java/rs/raf/rafeisen/MainActivity.kt
@@ -3,19 +3,32 @@ package rs.raf.rafeisen
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.FragmentActivity
 import dagger.hilt.android.AndroidEntryPoint
 import rs.raf.rafeisen.navigation.AppNavigation
+import rs.raf.rafeisen.splash.SplashViewModel
 import rs.raf.rafeisen.ui.theme.RAFeisenTheme
 
 @AndroidEntryPoint
 class MainActivity : FragmentActivity() {
+
+    private val splashViewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { !splashViewModel.isAuthCheckComplete.value }
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RAFeisenTheme {
-                AppNavigation()
+                val isLoggedIn = splashViewModel.isLoggedIn.collectAsState()
+
+                AppNavigation(startDestination = if (isLoggedIn.value) "home" else "login")
             }
         }
     }
