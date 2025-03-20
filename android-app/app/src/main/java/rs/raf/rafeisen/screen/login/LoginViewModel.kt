10b50373bfedde8660bd52caad692def449c2df2
Marko Kocic <markokocic2000@gmail.com>
2025-03-20 21:09:44 +0100

    android/login: adjust colors, form behavior, add sideffects and uievents for handling login logic later

--- android-app/app/src/main/java/rs/raf/rafeisen/screen/login/LoginViewModel.kt
+++ android-app/app/src/main/java/rs/raf/rafeisen/screen/login/LoginViewModel.kt
@@ -1,10 +1,41 @@
 package rs.raf.rafeisen.screen.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import rs.raf.rafeisen.screen.login.LoginContract.SideEffect
+import rs.raf.rafeisen.screen.login.LoginContract.UiEvent
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel
-    @Inject
-    constructor() : ViewModel()
+class LoginViewModel @Inject constructor() : ViewModel() {
+
+    private val events: MutableSharedFlow<UiEvent> = MutableSharedFlow()
+    fun setEvent(event: UiEvent) = viewModelScope.launch { events.emit(event) }
+
+    private val _effects = Channel<SideEffect>()
+    val effects = _effects.receiveAsFlow()
+    private fun setEffect(effect: SideEffect) = viewModelScope.launch { _effects.send(effect) }
+
+    init {
+        observeEvents()
+    }
+
+    private fun observeEvents() =
+        viewModelScope.launch {
+            events.collect {
+                when (it) {
+                    is UiEvent.LoginRequest -> login(it.email, it.password)
+                }
+            }
+        }
+
+    private fun login(email: String, password: String) {
+        /* TODO: implement login logic */
+        setEffect(SideEffect.LoginSuccessful)
+    }
+}
