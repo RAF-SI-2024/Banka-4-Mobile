cf4b869e9f733257e3414a59fb481450d2c30025
Marko Kocic <markokocic2000@gmail.com>
2025-03-21 00:09:36 +0100

    android: implement login logic into LoginScreen

--- android-app/app/src/main/java/rs/raf/rafeisen/screen/login/LoginViewModel.kt
+++ android-app/app/src/main/java/rs/raf/rafeisen/screen/login/LoginViewModel.kt
@@ -5,14 +5,26 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
+import rs.raf.rafeisen.auth.AuthService
 import rs.raf.rafeisen.screen.login.LoginContract.SideEffect
 import rs.raf.rafeisen.screen.login.LoginContract.UiEvent
+import rs.raf.rafeisen.screen.login.LoginContract.UiState
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor() : ViewModel() {
+class LoginViewModel @Inject constructor(
+    private val authService: AuthService,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(UiState())
+    val state = _state.asStateFlow()
+    private fun setState(reducer: UiState.() -> UiState) = _state.getAndUpdate { it.reducer() }
 
     private val events: MutableSharedFlow<UiEvent> = MutableSharedFlow()
     fun setEvent(event: UiEvent) = viewModelScope.launch { events.emit(event) }
@@ -34,8 +46,17 @@ class LoginViewModel @Inject constructor() : ViewModel() {
             }
         }
 
-    private fun login(email: String, password: String) {
-        /* TODO: implement login logic */
-        setEffect(SideEffect.LoginSuccessful)
-    }
+    private fun login(email: String, password: String) =
+        viewModelScope.launch {
+            try {
+                setState { copy(isWorking = true) }
+                authService.login(email = email, password = password)
+                setEffect(SideEffect.LoginSuccessful)
+            } catch (error: Exception) {
+                /* TODO: implement better error handling logic */
+                Timber.w(error)
+            } finally {
+                setState { copy(isWorking = false) }
+            }
+        }
 }
