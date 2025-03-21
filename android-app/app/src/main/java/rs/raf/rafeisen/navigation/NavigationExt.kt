1dee6c7c51b3eddc4be67a20c75638659c754ffe
Marko Kocic <markokocic2000@gmail.com>
2025-03-21 02:50:05 +0100

    android: add fab on home screen

--- android-app/app/src/main/java/rs/raf/rafeisen/navigation/NavigationExt.kt
+++ android-app/app/src/main/java/rs/raf/rafeisen/navigation/NavigationExt.kt
@@ -3,5 +3,5 @@ package rs.raf.rafeisen.navigation
 import androidx.navigation.NavController
 
 fun NavController.navigateToLogin() = navigate("login")
-
+fun NavController.navigateToAddTotp() = navigate("totp")
 fun NavController.navigateToHome() = navigate("home")
