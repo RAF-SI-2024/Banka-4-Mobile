3fe1992b02f67e2e240ef17aa0da1003114a733e
aaleksaaleksic <aaleksic9722rn@raf.rs>
2025-03-26 10:52:37 +0100

    rename LandingContract propreties

--- android-app/app/src/main/java/rs/raf/rafeisen/screen/landing/screen/LandingContract.kt
+++ android-app/app/src/main/java/rs/raf/rafeisen/screen/landing/screen/LandingContract.kt
@@ -4,14 +4,14 @@ import rs.raf.rafeisen.screen.home.mock.AccountMock
 import rs.raf.rafeisen.screen.home.mock.TransactionMock
 
 interface LandingContract {
-    data class LandingState(
+    data class LandingUIState(
         val isLoading: Boolean = true,
         val error: String? = null,
         val account: AccountMock? = null,
         val transactions: List<TransactionMock> = emptyList()
     )
 
-    sealed class LandingEvent {
-        object FetchData : LandingEvent()
+    sealed class LandingUIEvent {
+        object LoadAccountAndCardsData : LandingUIEvent()
     }
 }
